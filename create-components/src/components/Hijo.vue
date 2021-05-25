72d4594a32869bc28e9ce873df07c332220b7c80
jjrg77 <jjrgmadrid@gmail.com>
2021-05-25 18:48:05 +0200

    tarea 1 módulo 10.2

---
+++ create-components/src/components/Hijo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Hijo">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            message: 'Soy el componente hijo'
+        }
+    },
+}
+</script>
+
+<style scoped>
+    p {
+        color: red;
+        background: lightgrey;
+    }
+</style>
---
+++ create-components/src/components/Padre.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="Padre">
+        <p>{{ message }}</p>
+
+        <Hijo/>
+    </div>
+</template>
+
+<script>
+
+import Hijo from '@/components/Hijo.vue'
+
+export default {
+    data () {
+        return {
+            message: 'Soy el componente padre'
+        }
+    },
+
+    
+    components: {
+        Hijo
+    }
+}
+</script>
+
+
+<style scoped>
+    p {
+        color: blue;
+        background: grey;
+    }
+</style>
+
+
